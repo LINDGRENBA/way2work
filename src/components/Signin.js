3e72665df2b3ebfa6ecd7e1a62ccac80fc0f119f
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-09-22 11:07:22 -0700

    Update with sign up, sign in and log out functionality.
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,8 +1,67 @@
 import React from 'react';
+import firebase from 'firebase/app';
 
 function Signin (){
+  function doSignUp(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(function() {
+      console.log("successfully signed up!"); //update this part to notify user instead of console.log
+    }).catch(function(error) {
+      console.log(error.message); //update this part to notify user instead of console.log
+    });
+  }
+
+  function doSignIn(event) {
+    event.preventDefault();
+    const email = event.target.signinPassword.value;
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+      console.log("Successfully signed in!");
+    }).catch(function(error) {
+      console.log(error.message);
+    });
+  }
+
+  function doSignOut() {
+    firebase.auth().signOut().then(function() {
+      console.log("Successfully signed out!");
+    }).catch(function(error) {
+      console.log(error.message);
+    });
+  }
+  
   return (
-    <h1>Sign In</h1>
+    <React.Fragment>
+      <h1>Sign Up</h1>
+      <form onSubmit={doSignUp}>
+        <input
+          type="text"
+          name="email"
+          placeholder="email" />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password" />
+        <button type="submit">Sign up</button>
+      </form>
+
+      <h1>Sign In</h1>
+      <form onSubmit={doSignIn}>
+        <input
+          type="text"
+          name="signinEmail"
+          placeholder="email" />
+        <input
+          type="password"
+          name="signinPassword"
+          placeholder="Password" />
+        <button type="submit">Sign in</button>
+      </form>
+
+      <h1>Sign Out</h1>
+      <button onClick={doSignOut}>Sign Out</button>
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
+import { Provider } from 'react-redux';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import 'firebase/auth';
+
+const store = createStore(rootReducer);
+
+const rrfProps = {
+  firebase,
+  config: {
+    userProfile: "users",
+    useFirestoreForProfile: true,
+  },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
   document.getElementById('root')
 );
 
